30ae1c78a267cdecadd3f85f9a90bf00e1cf617c
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-07-02 17:36:50 +0900

    feat(error code): 요청으로 잘못된 eventType, action 을 보낼 경우 validationException 발생

--- src/main/java/com/triple/point/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/triple/point/exception/handler/GlobalExceptionHandler.java
@@ -2,16 +2,19 @@ package com.triple.point.exception.handler;
 
 import com.triple.point.ApiResponse;
 import com.triple.point.exception.errorCode.ErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ApiResponse<Object> handleEnum(HttpMessageNotReadableException e) {
-        return ApiResponse.error(ErrorCode.ENUM_VALIDATION_EXCEPTION.getCode(), ErrorCode.ENUM_VALIDATION_EXCEPTION.getMessage());
+        return ApiResponse.error(ErrorCode.VALIDATION_EXCEPTION.getCode(), ErrorCode.VALIDATION_EXCEPTION.getMessage());
     }
 
 }
